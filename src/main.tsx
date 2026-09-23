2843137c9420013acce6dd5e29b66bb4e1239791
amalvkiitil <amal.vk@iitil.com>
2026-09-23 13:52:36 +0530

    Bug fixes

--- src/main.tsx
+++ src/main.tsx
@@ -16,7 +16,7 @@ createRoot(document.getElementById('root')!).render(
           toastOptions={{
             duration: 2600,
             style: {
-              background: '#14172a',
+              background: '#1c1917',
               color: '#fff',
               fontSize: '14px',
               fontWeight: 600,
